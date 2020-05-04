002be73285b9015efb57078fae56102f7f94c366
Wangdi Tenzing <wangdi457@gmail.com>
2020-05-03 22:31:22 -0400

    Wrote a test for the get endpoint to check the status of the server

---
+++ api/server.spec.js
@@ -0,0 +1,17 @@
+const request= require("supertest")
+
+const server=require("./server.js")
+
+
+ describe("GET", ()=>{
+     it ("should return 200 ok", ()=>{
+         return request(server)
+         .get("/")
+         .then(res=>{
+             expect(res.status).toBe(200)
+             expect(res.body.message).toBe("server is up and running")
+             
+         })
+     })
+ })
+
